bb34d07a6b67f03875eb09a9fcc242bf9febba6d
heber59 <hesan0308@gmail.com>
2026-04-08 16:03:10 -0500

    feat: start astro proyect and workflow for deploy

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  // SUSTITUYE por tu dominio .com cuando lo tengas
+  site: "https://tu-usuario.github.io",
+  base: "/", // Si es el repositorio principal, usa '/'.
+  output: "static",
+});
