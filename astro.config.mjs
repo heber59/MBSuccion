2beab48b395ec41b8ad1b64e12e2d0aab92e6360
heber59 <hesan0308@gmail.com>
2026-04-08 16:09:55 -0500

    fix: adjust deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   // SUSTITUYE por tu dominio .com cuando lo tengas
-  site: "https://tu-usuario.github.io",
+  site: "https://heber59.github.io",
   base: "/", // Si es el repositorio principal, usa '/'.
   output: "static",
 });
