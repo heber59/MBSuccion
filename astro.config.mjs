e30a465d8414029eed151e59e610a72675b89a55
heber59 <hesan0308@gmail.com>
2026-05-02 10:58:30 -0500

    feat: first version of mbsuccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   // SUSTITUYE por tu dominio .com cuando lo tengas
   site: "https://heber59.github.io",
   base: "/", // Si es el repositorio principal, usa '/'.
   output: "static",
+  integrations: [sitemap()],
 });
