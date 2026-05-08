17706cb051153e9e9e109bf75722afe89bf9f210
heber59 <hesan0308@gmail.com>
2026-05-08 16:22:25 -0500

    fix: move to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  // SUSTITUYE por tu dominio .com cuando lo tengas
-  site: "https://heber59.github.io",
-  base: "/", // Si es el repositorio principal, usa '/'.
+  site: "",
+
   output: "static",
   integrations: [sitemap()],
 });
