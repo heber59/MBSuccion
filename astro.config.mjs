2a089efd3fa30dfb98ecb804c4950335959461b7
heber59 <hesan0308@gmail.com>
2026-05-08 17:19:13 -0500

    feat: adjust theme and some styles on main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,5 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "",
-
   output: "static",
-  integrations: [sitemap()],
 });
